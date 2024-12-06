f04e68696803158f26d9064af3d69a5228476757
Albincaz <albinca9207@gmail.com>
2024-12-06 10:55:14 +0530

    nodde

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const http = require("http")
+const fs = require("fs")
+const url = require("url")
+const PORT = 3005
+
+
+const app = http.createServer((req,res)=>{
+
+    const {pathname} = url.parse(req.url);
+    console.log(pathname);
+
+    if (pathname == "/"){
+        res.writeHead(200,{"Content-type" : "text/html"});
+        res.end(fs.readFileSync("../frontend"))
+    }
+    else if (pathname == "/css/index.css"){
+        res.writeHead(200,{"Content-type" : "text/css"});
+        res.end(fs.readFileSync("../frontend/css/index.css"))
+    }
+    else if (pathname == "/js/index.js"){
+        res.writeHead(200,{"Content-type" : "text/js"});
+        res.end(fs.readFileSync("../frontend/js/index.js"))
+    }
+})
+app.listen(PORT,()=>{
+    console.log("server created");
+})
