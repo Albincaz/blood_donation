974c0853ea026cb65efe42b72e607bac13a24466
Albincaz <albinca9207@gmail.com>
2024-12-07 11:24:36 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,38 @@
 const http = require("http")
 const fs = require("fs")
 const url = require("url")
-const path = require("path")
-const PORT = 3008
-
+const PORT = 3001
 
 const app = http.createServer((req,res)=>{
-
     const {pathname} = url.parse(req.url);
     console.log(pathname);
 
-    // if (pathname == "/"){
-    //     res.writeHead(200,{"Content-type" : "text/html"});
-    //     res.end(fs.readFileSync("../frontend"))
-    // }
-    // else if (pathname == "/css/index.css"){
-    //     res.writeHead(200,{"Content-type" : "text/css"});
-    //     res.end(fs.readFileSync("../frontend/css/index.css"))
-    // }
-    // else if (pathname == "/js/index.js"){
-    //     res.writeHead(200,{"Content-type" : "text/js"});
-    //     res.end(fs.readFileSync("../frontend/js/index.js"))
-    // }
+if (pathname=="/"){
+    res.writeHead(200,{"Content-type" : "text/html"});
+    res.end(fs.readFileSync("../frontend/index.html"))
+}
+
+else if (pathname=="/css/index.css"){
+    res.writeHead(200,{"Content-type" : "text/css"});
+    res.end(fs.readFileSync("../frontend/css/index.css"))
+}
+else if (pathname=="/js/index.js"){
+    res.writeHead(200,{"Content-type" : "text/js"});
+    res.end(fs.readFileSync("../frontend/js/index.js"))
+}
+
+else if (pathname=="/pages/adddonor.html"){
+    res.writeHead(200,{"Content-type" : "text/html"});
+    res.end(fs.readFileSync("../frontend/pages/adddonor.html"))
+}
+
+else if (pathname=="/css/adddonor.css"){
+    res.writeHead(200,{"Content-type" : "text/css"});
+    res.end(fs.readFileSync("../frontend/css/adddonor.css"))
+}
+
 })
+
 app.listen(PORT,()=>{
     console.log("server created");
 })
